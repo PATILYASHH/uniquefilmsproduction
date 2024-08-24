59dca5fadb197424a61ebc8009ca9a08f89222aa
Yash patil <149749700+PATILYASHH@users.noreply.github.com>
2024-08-24 10:08:13 +0000

    fgdfdg

---
+++ script.js
@@ -0,0 +1,7 @@
+// Preloader functionality
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+      document.getElementById('preloader').style.display = 'none';
+    }, 2000); // Adjust timing if needed
+  });
+  
